a3e711e0a67ca584c28a943f0cfa6ce312b39f55
zengchang233 <zengchang@nii.ac.jp>
2021-04-30 01:05:16 +0900

    update nnet_trainer

--- libs/trainer/nnet_trainer.py
+++ libs/trainer/nnet_trainer.py
@@ -1,23 +1,202 @@
+import sys
+sys.path.insert(0,"/groups1/gcc50479/spk/code/asv_beginner")
+
 import torch
+import libs.dataio.dataset as dataset
 
 class NNetTrainer(object):
-    def __init__(self):
+    def __init__(self, opts):
+        self.train_opts = opts['train']
+        self.model_opts = opts['model']
+        self.data_opts = opts['data']
+
+        args = vars(self.parse_args())
+        self.data_opts[self.data_opts['data_format']]['feat_type'] = args['feat_type']
+        self.model_opts['arch'] = args["arch"]
+        self.model_opts[args['arch']]['input_dim'] = args['input_dim']
+        self.train_opts['loss'] = args["loss"]
+        self.train_opts['bs'] = args['bs']
+        self.train_opts['collate'] = args['collate']
+        self.train_opts['device'] = args['device']
+        if "resume" in args:
+            self.train_opts['resume'] = args["resume"]
+
+        if os.path.exists(self.train_opts['resume']):
+            self.log_time = self.train_opts['resume'].split('/')[1]
+        else:
+            self.log_time = time.asctime(time.localtime(time.time())).replace(' ', '_').replace(':', '_')
+        logging.info(self.log_time)
+
+        if self.data_opts['data_format'] == 'kaldi': 
+            self.trainset = dataset.SpeechTrainDataset(self.data_opts)
+        logging.info("Using {} to extract features".format(self.data_opts['data_format']))
+        logging.info("Using {} feature".format(args['feat_type']))
+
+        n_spk = self.trainset.n_spk
+
+        if self.model_opts['arch'] == 'tdnn' or self.model_opts['arch'] == 'etdnn':
+            import components.models.tdnn as tdnn
+            model = tdnn.SpeakerEmbNet(self.model_opts)
+        elif self.model_opts['arch'] == 'resnet':
+            import components.models.resnet as resnet
+            model = resnet.SpeakerEmbNet(self.model_opts)
+        else:
+            raise NotImplementedError("Other models are not implemented!")
+        logging.info("Using {} neural network".format(self.model_opts['arch']))
+
+        device_num = 1
+
+        if self.train_opts['device'] == 'gpu':
+            self.device = torch.device('cuda')
+            device_ids = self.train_opts['gpus_id']
+            device_num = len(device_ids)
+            self.train_opts['check_interval'] = self.train_opts['check_interval'] // device_num
+            self.model = torch.nn.DataParallel(model.to(self.device), device_ids = device_ids)
+            logging.info("Using GPU: {}".format(device_ids))
+        else:
+            self.device = torch.device('cpu')
+            logging.info('Using CPU')
+
+        if self.train_opts['collate'] == 'length_varied':
+            train_collate_fn = self.trainset.collate_fn
+            logging.info("Length of input utterances in different batch is varying")
+        elif self.train_opts['collate'] == 'kaldi':
+            train_collate_fn = self.trainset.kaldi_collate_fn
+            logging.info("Length of input utterances is fixed")
+        else:
+            raise NotImplementedError("Other collate method are not implemented!")
+
+        self.embedding_dim = self.model_opts[self.model_opts['arch']]['embedding_dim']
+        logging.info("Dimension of speaker embedding is {}".format(self.embedding_dim))
+
+        if self.train_opts['loss'] == 'CrossEntropy':
+            self.criterion = CrossEntropy(self.embedding_dim, n_spk).to(self.device)
+        elif self.train_opts['loss'] == 'LMCL':
+            margin_range = self.train_opts['margin']
+            self.init_margin = margin_range[0]
+            self.end_margin = margin_range[1]
+            if self.model_opts['arch'] in ['tdnn', 'etdnn']:
+                self.criterion = LMCL(self.embedding_dim, n_spk, self.train_opts['scale'], self.init_margin).to(self.device)
+            elif self.model_opts['arch'] == 'resnet':
+                self.criterion = LMCL(self.embedding_dim, n_spk, self.train_opts['scale'], self.end_margin).to(self.device)
+        elif self.train_opts['loss'] == 'LMCL_Uniform':
+            margin_range = self.train_opts['margin']
+            self.init_margin = margin_range[0]
+            self.end_margin = margin_range[1]
+            self.criterion = LMCLUniformLoss(self.embedding_dim, n_spk, self.train_opts['scale'], self.end_margin).to(self.device)
+        else:
+            raise NotImplementedError("Other loss function has not been implemented yet!")
+        logging.info('Using {} loss function'.format(self.train_opts['loss']))
+
+        param_groups = [{'params': self.model.parameters()}, {'params': self.criterion.parameters()}]
+        
+        if self.train_opts['type'] == 'sgd':
+            optim_opts = self.train_opts['sgd']
+            self.optim = optim.SGD(param_groups, optim_opts['init_lr'], momentum = optim_opts['momentum'], weight_decay = optim_opts['weight_decay'])
+        elif self.train_opts['type'] == 'adam':
+            optim_opts = self.train_opts['adam']
+            self.optim = optim.Adam(param_groups, optim_opts['init_lr'], weight_decay = optim_opts['weight_decay'])
+        logging.info("Using {} optimizer".format(self.train_opts['type']))
+
+        self.epoch = self.train_opts['epoch']
+        logging.info("Total train {} epochs".format(self.epoch))
+
+        self.trainloader = DataLoader(self.trainset, shuffle = True, collate_fn = train_collate_fn, batch_size = self.train_opts['bs'] * device_num, num_workers = 32, pin_memory = True)
+        self.voxtestloader = DataLoader(self.voxtestset, batch_size = 1, shuffle = False, num_workers = 8, pin_memory = True)
+        self.lr_scheduler = lr_scheduler.ReduceLROnPlateau(self.optim, mode = self.train_opts['lr_scheduler_mode'], 
+                                                           factor = self.train_opts['lr_decay'], patience = self.train_opts['patience'], 
+                                                           min_lr = self.train_opts['min_lr'], verbose = True)
+        self.current_epoch = 0
+
+        if os.path.exists(self.train_opts['resume']):
+            self.load(self.train_opts['resume'])
+
+    def _move_to_device(self):
         pass
+
+    def parse_args(self):
+        parser = argparse.ArgumentParser(description = "specify some important arguments")
+        parser.add_argument("--feat-type", type = str, default = 'mfcc', dest = "feat_type", help = 'input feature')
+        parser.add_argument("--input-dim", type = int, default = 30, dest = "input_dim", help = "dimension of input feature")
+        parser.add_argument("--arch", type = str, default = "tdnn", choices = ["resnet", "tdnn", "etdnn", "ftdnn", "rawnet", "wav2spk"], help = "specify model architecture")
+        parser.add_argument("--loss", type = str, default = "AMSoftmax", choices = ["AMSoftmax", "CrossEntropy", "ASoftmax", "TripletLoss"], help = "specify loss function")
+        parser.add_argument("--bs", type = int, default = 64, help = "specify batch size for training")
+        parser.add_argument("--collate", type = str, default = "length_varied", choices = ["kaldi", "length_varied"], help = "specify collate function in DataLoader")
+        parser.add_argument("--resume", type = str, help = "if you give a ckpt path to this argument and if the ckpt file exists, it will resume training based on this ckpt file. Otherwise, it will start a new training process")
+        parser.add_argument("--device", default = 'gpu', choices = ['gpu', 'cpu'], help = 'designate the device on which the model will run')
+        return parser.parse_args()
+
+    def model_average(self, avg_num = 4):
+        model_state_dict = {}
+        for i in range(avg_num):
+            suffix = self.current_epoch - i
+            ckpt = torch.load('exp/{}/net_{}.pth'.format(self.log_time, suffix))
+            state_dict = ckpt['state_dict']
+            for k, v in state_dict.items():
+                if k in model_state_dict:
+                    model_state_dict[k] += v
+                else:
+                    model_state_dict[k] = v
+        for k, v in model_state_dict.items():
+            model_state_dict[k] = v / avg_num
+        torch.save({'epoch': 0, 'state_dict': model_state_dict,
+                    'optimizer': ckpt['optimizer']},
+                    'exp/{}/net_avg.pth'.format(self.log_time))
+        self.model.load_state_dict(model_state_dict)
+
+    def save(self, filename = None):
+        if filename is None:
+            torch.save({'epoch': self.current_epoch, 'state_dict': self.model.state_dict(), 'criterion': self.criterion,
+                        'lr_scheduler': self.lr_scheduler.state_dict(), 'optimizer': self.optim.state_dict()},
+                        'exp/{}/net_{}.pth'.format(self.log_time, self.current_epoch))
+        else:
+            torch.save({'epoch': self.current_epoch, 'state_dict': self.model.state_dict(), 'criterion': self.criterion,
+                        'lr_scheduler': self.lr_scheduler.state_dict(), 'optimizer': self.optim.state_dict()},
+                        'exp/{}/{}'.format(self.log_time, filename))
+
+    def load(self, resume):
+        ckpt = torch.load(resume)
+        self.model.load_state_dict(ckpt['state_dict'])
+        if 'criterion' in ckpt:
+            self.criterion = ckpt['criterion']
+        if 'lr_scheduler' in ckpt:
+            self.lr_scheduler.load_state_dict(ckpt['lr_scheduler'])
+        self.optim.load_state_dict(ckpt['optimizer'])
+        self.current_epoch = ckpt['epoch']
+
+    def __call__(self):
+        os.makedirs('exp/{}'.format(self.log_time), exist_ok = True)
+        if not os.path.exists("exp/{}/config.yaml".format(self.log_time)):
+            with open("exp/{}/config.yaml".format(self.log_time), 'w') as f:
+                yaml.dump(self.data_opts, f)
+                yaml.dump(self.model_opts, f)
+                yaml.dump(self.train_opts, f)
+        # train, test 
+        logging.info("start training")
+        self.train()
+        self.model_average(3)
 
     def train(self):
-        pass
+        start_epoch = self.current_epoch
+        self.best_dev_epoch = self.current_epoch
+        self.best_dev_loss = 1000
+        self.count = 0
+        self.dev_check_count = 0
+        for epoch in range(start_epoch + 1, self.epoch + 1):
+            self.current_epoch = epoch
+            logging.info("Epoch {}".format(self.current_epoch))
+            if self.model_opts['arch'] in ['tdnn', 'etdnn']:
+                self._adjust_margin()
+            stop = self._train_epoch()
+            self.save()
+            if stop == -1:
+                break 
 
     def train_epoch(self):
-        pass
+        raise NotImplementedError("Please implement extract_embedding function by yourself!")
 
     def _dev(self):
-        pass
+        raise NotImplementedError("Please implement extract_embedding function by yourself!")
 
     def extract_embedding(self):
-        pass
-
-    def save(self, filepath = None):
-        pass
-
-    def load(self):
-        pass
+        raise NotImplementedError("Please implement extract_embedding function by yourself!")
