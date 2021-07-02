597cf0068aea12e2eca1eb79d9b2eeb8e3b126cb
zengchang233 <zengchang@nii.ac.jp>
2021-07-03 08:16:45 +0900

    add etdnn demo

---
+++ demos/voxceleb/etdnn/local/evaluation.py
@@ -0,0 +1,101 @@
+import os
+import sys
+sys.path.insert(0, "../../../")
+import argparse
+
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from tqdm import tqdm
+
+from eval_dataset import SpeechEvalDataset
+from libs.utils.performance_eval import compute_eer
+from libs.utils.utils import read_config
+from libs.nnet import tdnn
+
+class Evaluator(object): 
+    def __init__(self, data_opts, model_opts, args): 
+        eval_dataset = SpeechEvalDataset(data_opts)
+        self.input_dim = eval_dataset[0][0].size(0)
+        self.evalloader = DataLoader(eval_dataset, batch_size = 1, shuffle = False, num_workers = 8, pin_memory = True)
+        self.build_model(model_opts)
+        self.load_model(args)
+        self.data_opts = data_opts
+        self.model_opts = model_opts
+        self.location = args['l']
+
+    def build_model(self, model_opts):
+        #  model_config = read_config("conf/model/{}.yaml".format(model_opts['arch']))
+        model_config = model_opts
+        model_config['input_dim'] = self.input_dim
+        self.embedding_dim = model_config['embedding_dim']
+        self.model = tdnn.XVector(model_config)
+
+    def load_model(self, args): 
+        self.device = torch.device(args['d']) 
+        self.exp_dir = args['e']
+        model_file_name = args['m']
+        ckpt = torch.load('exp/{}/{}'.format(self.exp_dir, model_file_name), map_location = self.device)
+        self.model.load_state_dict(ckpt['state_dict'])
+        self.model.to(self.device)
+
+    def extract_embedding(self, feature): 
+        feature = feature.to(self.device)
+        if self.location == 'near': 
+            _, xv = self.model.extract_embedding(feature)
+        elif self.location == 'far': 
+            xv, _ = self.model.extract_embedding(feature)
+            xv = F.normalize(xv)
+        return xv
+
+    def compute_cosine_score(self): 
+        y_true = []
+        y_pred = []
+        score_file = open('exp/{}/scores.txt'.format(self.exp_dir), 'w')
+        with open('./task.txt', 'r') as f:
+            for line in tqdm(f):
+                line = line.rstrip()
+                true_score, test_utt1, test_utt2 = line.split(' ')
+                y_true.append(eval(true_score))
+                utt1_feat = np.load(os.path.join('exp/{}/test_xv'.format(self.exp_dir), test_utt1.replace('.wav', '.npy')))
+                utt2_feat = np.load(os.path.join('exp/{}/test_xv'.format(self.exp_dir), test_utt2.replace('.wav', '.npy')))
+                score = cosine_similarity(utt1_feat.reshape(1, -1), utt2_feat.reshape(1, -1)).reshape(-1)
+                y_pred.append(score)
+                score_file.write(str(score[0]) + ' ' + test_utt1 + ' ' + test_utt2 + '\n')
+        score_file.close()
+        return y_true, y_pred
+
+    def evaluate(self):
+        self.model.eval()
+        os.makedirs('exp/{}/test_xv'.format(self.exp_dir), exist_ok = True)
+        with torch.no_grad():
+           for feature, utt in tqdm(self.evalloader):
+               utt = utt[0]
+               feature = feature.to(self.device)
+               xv = self.extract_embedding(feature)
+               xv = xv.cpu().numpy()
+               test_spk_dir = os.path.join('exp/{}/test_xv'.format(self.exp_dir), os.path.dirname(utt))
+               os.makedirs(test_spk_dir, exist_ok = True)
+               np.save(os.path.join(test_spk_dir, os.path.basename(utt).replace('.wav', '.npy')), xv)
+        y_true, y_pred = self.compute_cosine_score()
+        eer, threshold = compute_eer(y_true, y_pred)
+        print("EER       : {:.4%}".format(eer))
+        print("Threshold : {:.4f}".format(threshold))
+
+def main(): 
+    parser = argparse.ArgumentParser("options for evaluation of speaker verification")
+    parser.add_argument("-e", "--exp-dir", dest = 'e', type = str, help = "the name of experiment directory")
+    parser.add_argument("-m", "--model", dest = 'm', type = str, help = "the evaluation model")
+    parser.add_argument("-d", "--device", dest = 'd', type = str, choices = ['cuda', 'cpu'], help = "device name")
+    parser.add_argument("-l", "--location", dest = 'l', type = str, choices = ['near', 'far'], help = "which to extract embedding")
+    args = parser.parse_args()
+    args = vars(args)
+    data_config = read_config("exp/{}/conf/data.yaml".format(args['e']))
+    model_config = read_config("exp/{}/conf/model.yaml".format(args['e']))
+    evaluator = Evaluator(data_config, model_config, args)
+    evaluator.evaluate()
+
+if __name__ == "__main__": 
+    main()
---
+++ demos/voxceleb/etdnn/run.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+alias python=`which python3`
+
+stage=1
+# train a frontend module
+if [ $stage -le 0 ]; then
+    # options:
+    # --feat-type {python,kaldi}_{fbank,mfcc,spectrogram}
+    # --input-dim 1 for resnet, feature dimension for xvector
+    # --arch resnet, tdnn, etdnn, ... take ../../../libs/nnet/*.py as reference
+    # --loss CrossEntropy, AMSoftmax, TripletLoss, ...
+    # --bs batch size
+    # --resume resume path
+    # --device gpu or cpu
+    # --mode depreciated
+    python local/nnet/trainer.py --feat-type kaldi_fbank --arch etdnn --input-dim 80 \
+        --device cuda --bs 64 --loss AMSoftmax # --resume exp/Sun_Jun_27_02_23_32_2021/net_1.pth
+    echo "frontend training done!"
+    exit 0;
+fi
+
+##### Result #####
+# model config: 
+    # TDNN layers: [512,512,512,512,512,512,512,512,512,1500], [[-2,-1,0,1,2],[0],[-2,0,2],[0],[-3,0,3],[0],[-4,0,4],[0],[0],[0]]
+    # FC layers: fc1+activation+bn, fc2 (without activation and bn), fc3 + softmax
+# feature: 80 dims kaldi fbank (log = true)
+# loss: AMSoftmax (s=20,m=0.2)
+# voxceleb1 dev as training set, voxceleb1 test as test set
+    # no augmentation, no repeat: 5.15%
+    # no augmentation, repeat: % (xvector in kaldi EER is 5.302% as reference)
+    # augmentation, no repeat: 3.86% (xvector in kaldi EER is 5.302% as reference)
+    # augmentation, repeat: % (xvector in kaldi EER is 5.302% as reference)
+# voxceleb2 dev as training set (no voxceleb2 test set)
+    # no augmentation, no repeat: 2.86%
+# voxceleb2 dev as training set
+    # no augmentation, no repeat: 
+
+# evaluation on test set without backend (or using cosine backend)
+if [ $stage -le 1 ]; then
+    expdir=$1
+    start=$2
+    stop=$3
+    for x in `seq $start $stop`; do
+        python local/evaluation.py -e $expdir -m net_${x}.pth -d cuda -l far
+        # python local/evaluation.py -e $expdir -m best_dev_model.pth -d cuda -l far
+    done
+    echo "scoring with only frontend done!"
+    exit 0;
+fi
+
+# train a backend module
+# if [ $stage -le 1 ]; then
+#     python local/backend/backend_trainer.py
+# fi
+
+# evaluation on test set
+if [ $stage -le 2 ]; then
+    python local/evaluation.py -e Sat_May__8_11_24_59_2021 -m best_dev_model.pth -d gpu -l far
+fi 
